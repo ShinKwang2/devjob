7cd56224e18f4c40cce1ce2baf5f687c1201b6c6
ShinKwang2 <shinkwang@yonsei.ac.kr>
2026-09-08 11:03:28 +0900

    init: devjob project

---
+++ src/main/java/com/shinkwang/devjob/DevjobApplication.java
@@ -0,0 +1,13 @@
+package com.shinkwang.devjob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevjobApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevjobApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shinkwang/devjob/DevjobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shinkwang.devjob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevjobApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
